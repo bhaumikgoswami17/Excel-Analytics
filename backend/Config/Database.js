3c2049205c97d429bdc8491660b65cbd672ee678
bhaumikgoswami17 <bhaumikgoswami4545@gmail.com>
2025-06-03 18:37:28 +0530

    first commit

--- backend/Config/Database.js
+++ backend/Config/Database.js
@@ -10,7 +10,7 @@ export const connect = () => {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     })
-    .then(() => console.log("DB Connection Success"))
+    .then(() => console.log("Data Base Connection Success"))
     .catch((err) => {
       console.log("DB Connection Failed");
       console.log(err);
